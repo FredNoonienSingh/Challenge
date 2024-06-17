d550316234002931eaea3895e3d2216872507a26
Perfectum Computersysteme <Baumeister.Fred@Outlook.com>
2024-06-17 23:44:55 +0200

    restructuring and added setup.py

--- test.py
+++
@@ -1,3 +0,0 @@
-# author :   Frederic Baumeister
-# date   :   15th june 2024 
-# Project:   Python Car Management RESTful API Coding Challenge
--- utils/create_db.py
+++ utils/create_db.py
@@ -2,38 +2,18 @@
 # date   :   15th june 2024 
 # Project:   Python Car Management RESTful API Coding Challenge
 
-import os 
+from .car import Car
 from datetime import datetime 
 
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine, Column, String, DateTime, Numeric, Integer
 
+
 Base = declarative_base()
-
-class Car(Base):
-    """ class containing the representation of a Car in the DB
-    """
-    __tablename__ = 'cars'
-
-    id = Column(Integer, primary_key=True)
-    make = Column(String(15), nullable=False)
-    model = Column(String(15), nullable=False)
-    year = Column(Numeric(precision=4, scale=0), nullable=False)
-        # Technically a float but limiting the scale to 0 it behaves like an Integer, 
-        # i choose to do it because it allows me the limit the year to a length to 4 digits 
-    color = Column(String(15), nullable=False)
-    price = Column(Numeric(precision=10, scale=2), nullable=False)
-    created_at = Column(DateTime, default=datetime.now)
-
 
 if __name__ == "__main__":
     DB_PATH = "database.db"
-    
-    try:
-        f = open(f"./{DB_PATH}", "x")
-    except Exception as e: 
-        print(e)
-    
+ 
     engine = create_engine(f'sqlite:///{DB_PATH}')
     Base.metadata.create_all(engine)
 
